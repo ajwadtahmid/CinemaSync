cb8d31ddaf0a06d64bb38be06bdc09008b5677f4
Ajwad Tahmid <dev@ajwadtahmid.com>
2026-04-25 21:51:09 -0700

    chore: Initialize Flutter project with default configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cinema_sync"
+    namespace = "com.ajwadtahmid.cinema_sync"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cinema_sync"
+        applicationId = "com.ajwadtahmid.cinema_sync"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/com/ajwadtahmid/cinema_sync/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ajwadtahmid.cinema_sync
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
